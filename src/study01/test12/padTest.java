5576a3a3831d60e0904daf0723db5a91ecce7ccc
Administrator <Administrator@koitt03a-PC>
2019-10-11 14:36:45 +0900

    10.11

---
+++ src/study01/test12/padTest.java
@@ -0,0 +1,15 @@
+package study01.test12;
+
+public class padTest {
+	
+	public static void main(String[] args) {
+		String str= "1";
+		int max = 4-str.length();
+		for(int i=1;i<=max;i++) {
+			str += "0";
+			System.out.println(str);
+		}
+		
+	}
+
+}
